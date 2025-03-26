c5d84ccbbe279df2ec39ef2b3af7f1ae577db82d
spuersant271 <santiagoabad.clementearredondo.gen23@cbtis105.edu.mx>
2025-03-26 10:13:51 -0600

    completado con exito

--- Models/personajesMongo.cs
+++ Models/personajesMongo.cs
@@ -3,27 +3,36 @@ using MongoDB.Bson.Serialization.Attributes;
 public class PersonajeMongo {
     [BsonId]
     [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
-
     public string? Id { get; set; }
-    public string? Id_experimento { get; set; }
-    public string Nombre { get; set; }
-    public bool Nombre_enemigo { get; set; }
-    public bool cientifico_principal { get; set; }
-    public string? nivel_peligro { get; set; }
-    public string Rol { get; set; }
-    public string Genero { get; set; }
-    public int? Edad { get; set; }
-    public decimal descripcion { get; set; }
-    public string estado { get; set; }
-    public bool armadura { get; set; }
-    public bool salud { get; set; }
-    public bool velocidad { get; set; }
-    public bool daño { get; set; }
-    public bool es_jefe { get; set; }
-    public bool es_volador { get; set; }
+
+    public int? id_experimento { get; set; }
+    public string nombre { get; set; } = "";
+    public string cientifico { get; set; }
+    public int? peligro { get; set; }
+    public string? eguridad { get; set; }
+    public string rol { get; set; } = "";
+    public string genero { get; set; } = "";
+    public int? edad { get; set; }
+    public string descripcion { get; set; } = "";
+    public string estado { get; set; } = "";
+    public int? armadura { get; set; }
+    public int salud { get; set; }
+    public decimal? velocidad { get; set; }
+    public int? daño { get; set; }
+    public bool jefe { get; set; }
+    public bool? es_volador { get; set; }
+    public bool? volador { get; set; }
     public bool agresivo { get; set; }
-    public bool fecha_realizacion { get; set; }
+    public string fecha { get; set; }
     public bool exitoso { get; set; }
-    public bool costo { get; set; }
-    
+    public decimal costo { get; set; }
+    public bool hacking { get; set; }
+    public bool aliado { get; set; }
+    public string? aparicion { get; set; }
+    public string? paradero { get; set; }
+    public string? trabajo { get; set; }
+    public string? apariciones { get; set; }
+    public string? habilidades { get; set; }
+    public decimal riesgo { get; set; }
+    public int? seguridad { get; set; }
 }
--- practica2/Conexion.cs
+++ practica2/Conexion.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
 
 [ApiController]
 [Route("conexion")]
@@ -6,6 +7,12 @@ public class Conexion : Controller {
     
     [HttpGet("mongo")]
     public IActionResult ListarSalonesMongoDb(){
-        return Ok("Me estoy conectando a MongoDB");
+        MongoClient client = new MongoClient(CadenasConexion.MONGO_DB);
+        var db = client.GetDatabase("practica1_Clemente_Santiago");
+        var collection = db.GetCollection<PersonajeMongo>("aliados y enemigos de half life y experimentos de black mesa");
+
+        var lista = collection.Find(FilterDefinition<PersonajeMongo>.Empty).ToList();
+
+        return Ok(lista);
     }
 }
